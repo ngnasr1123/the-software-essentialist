90754ed9359ba0747a9fa18cea250ee1f5c26cfb
Nader Nasr <nadergnasr@gmail.com>
2023-06-10 21:42:15 +0000

    [red] add knows mom is a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,18 @@
 
 describe('palindrome checker', () => {
-
+    it('knows "mom" is a palindrome', () => {
+        expect(palindromeChecker("mom")).toEqual(true);
+    });
 })
+
+/*
+"mom" returns true
+"Mom" returns true
+"MoM" returns true
+"Momx" returns false
+"xMomx" returns true
+"Was It A Rat I Saw" returns true
+"Never Odd or Even" returns true
+"Never Odd or Even1" returns false 
+"1Never Odd or Even1" returns true
+*/
