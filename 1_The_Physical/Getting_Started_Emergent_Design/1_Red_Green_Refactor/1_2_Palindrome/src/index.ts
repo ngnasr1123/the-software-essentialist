0eb3fb5296314b0c3eb75f96bada7259deafa709
Nader Nasr <nadergnasr@gmail.com>
2023-06-10 21:56:55 +0000

    [green] fix knows xMomx is a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,7 @@
 export function palindromeChecker(word: string): boolean {
     if (word.toLowerCase() === "mom") return true;
 
+    if (word.toLowerCase() === "xmomx") return true;
+
     return false;
 }
