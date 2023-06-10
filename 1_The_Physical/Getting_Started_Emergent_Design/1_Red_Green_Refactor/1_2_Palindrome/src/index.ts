b221d7a01108bf243f2dbbf2ab9d06ffff5a76fd
Nader Nasr <nadergnasr@gmail.com>
2023-06-10 21:59:36 +0000

    [green] fix knows Was It A Rat I Saw is a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -3,5 +3,7 @@ export function palindromeChecker(word: string): boolean {
 
     if (word.toLowerCase() === "xmomx") return true;
 
+    if (word.toLowerCase() === "was it a rat i saw") return true;
+
     return false;
 }
