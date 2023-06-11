fdf8e735d952ed1b04cce73d7d865c26e3345ef3
Nader Nasr <nadergnasr@gmail.com>
2023-06-11 17:22:11 +0000

    [red] add knows maxwellTheBe is missing digits

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -12,6 +12,13 @@ describe('password validator', () => {
     expect(output.errors).toHaveLength(1);
     expect(output.errors[0]).toEqual("MissingUppercase");
   });
+
+  it('knows maxwellTheBe is missing digits', () => {
+    const output = PasswordValidator.validate('maxwellTheBe');
+    expect(output.result).toBeFalsy();
+    expect(output.errors).toHaveLength(1);
+    expect(output.errors[0]).toEqual("MissingDigits");
+  });
 })
 
 
