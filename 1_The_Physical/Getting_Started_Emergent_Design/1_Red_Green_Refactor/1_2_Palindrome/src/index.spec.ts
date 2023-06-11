7ea93dec06ba4222b795ef702c60c1ea961246ff
Nader Nasr <nadergnasr@gmail.com>
2023-06-11 03:37:42 +0000

    [refactor] modify unit tests to use it.each

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,40 +1,23 @@
 import { palindromeChecker } from './index';
 
 describe('palindrome checker', () => {
-    it('knows "mom" is a palindrome', () => {
-        expect(palindromeChecker("mom")).toEqual(true);
+    it.each([
+        'mom',
+        'Mom',
+        'MoM',
+        'xMomx',
+        'Was It A Rat I Saw',
+        'Never Odd or Even',
+        '1Never Odd or Even1'
+    ])('Knows %p is a palindrome', (text) => {
+        expect(palindromeChecker(text)).toBeTruthy();
     });
 
-    it('knows "Mom" is a palindrome', () => {
-        expect(palindromeChecker("Mom")).toEqual(true);
-    });
-
-    it('knows "MoM" is a palindrome', () => {
-        expect(palindromeChecker("MoM")).toEqual(true);
-    });
-
-    it('knows "Momx" is a palindrome', () => {
-        expect(palindromeChecker("Momx")).toEqual(false);
-    });
-
-    it('knows "xMomx" is a palindrome', () => {
-        expect(palindromeChecker("xMomx")).toEqual(true);
-    });
-
-    it('knows "Was It A Rat I Saw" is a palindrome', () => {
-        expect(palindromeChecker("Was It A Rat I Saw")).toEqual(true);
-    });
-
-    it('knows "Never Odd or Even" is a palindrome', () => {
-        expect(palindromeChecker("Never Odd or Even")).toEqual(true);
-    });
-
-    it('knows "Never Odd or Even1" is a palindrome', () => {
-        expect(palindromeChecker("Never Odd or Even1")).toEqual(false);
-    });
-
-    it('knows "1Never Odd or Even1" is a palindrome', () => {
-        expect(palindromeChecker("1Never Odd or Even1")).toEqual(true);
+    it.each([
+        'Momx',
+        'Never Odd or Even1'
+    ])('Knows %p is not a palindrome', (text) => {
+        expect(palindromeChecker(text)).toBeFalsy();
     });
 })
 
