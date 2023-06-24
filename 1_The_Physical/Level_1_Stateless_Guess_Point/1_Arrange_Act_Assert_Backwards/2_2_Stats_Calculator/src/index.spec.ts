4bdaa0ef4e9342ff2353da7325fd24cc03675f32
Nader Nasr <nadergnasr@gmail.com>
2023-06-24 13:48:03 +0000

    [refactor] remove duplicate output variable

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -6,24 +6,22 @@ describe('stats calculator', () => {
         expect(typeof output).toBe('object');
     });
 
-    describe('when the sequence is 2, 4, 21, -8, 53, and 40', () => {
+    describe('when the sequence is 2, 4, 21, -8, 53, and 40', () => {   
+        const output = calculateStats([2, 4, 21, -8, 53, 40]);
+
         it('knows -8 is the minimum value', () => {
-            const output = calculateStats([2, 4, 21, -8, 53, 40]);
             expect(output.min).toEqual(-8);
         });
 
         it('knows 53 is the maximum value', () => {
-            const output = calculateStats([2, 4, 21, -8, 53, 40]);
             expect(output.max).toEqual(53);
         });
 
         it('knows the sequence has 6 numbers', () => {
-            const output = calculateStats([2, 4, 21, -8, 53, 40]);
             expect(output.count).toEqual(6);
         });
 
         it('knows 18.666666666667 is the average', () => {
-            const output = calculateStats([2, 4, 21, -8, 53, 40]);
             expect(output.avg).toEqual(18.666666666667);
         });
     });
