14f7b12b90d6d9a066bcf75d08dd6015cd57747c
Nader Nasr <nadergnasr@gmail.com>
2023-06-24 13:20:44 +0000

    [green] add returns an object test

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { calculateStats } from './index';
 
 describe('stats calculator', () => {
-
-
+    it('it returns an object', () => {
+        const output = calculateStats([]);
+        expect(typeof output).toBe('object');
+    });
 })
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,3 @@
+export function calculateStats(sequence: number[]): Object {
+    return {};
+}
